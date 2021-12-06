3b0fcf983580b149675e53f5673ea38ea727db7e
OnikenX <OnikenX@pm.me>
2021-12-06 12:09:36 +0000

    removed ridicle solutions and added funtions for part 1 and 2

--- src/d6.rs
+++ src/d6.rs
@@ -1,5 +1,3 @@
-use std::{os::unix::prelude::FileExt, fs::read_to_string};
-
 use crate::downloader::{self, get_token};
 
 // tempo interno de 3
@@ -9,9 +7,8 @@ use crate::downloader::{self, get_token};
 // depois de um dia seria 6 e cria um novo laternfish com o counter a 8
 //fish 1 - 5 / fish 2 - 7
 
-pub fn solver(days : i32){
-    let input = 
-    downloader::download(2021, 6, &get_token());
+pub fn solver(days: i32) {
+    let input = downloader::download(2021, 6, &get_token());
     //0/1/2/3/4/5/6/7/8
     let mut state = [0usize; 9];
     for item in input.split(',') {
@@ -21,14 +18,21 @@ pub fn solver(days : i32){
     }
     let mut prev = state[8];
     let mut actual = 0;
-    for day in 1..=days{
-        for index in (0..state.len()-1).rev(){
+    for day in 1..=days {
+        for index in (0..state.len() - 1).rev() {
             actual = state[index];
             state[index] = prev;
-            prev=actual;
+            prev = actual;
         }
         state[6] += actual;
         state[8] = actual;
     }
     println!("{:?} - {}", state, state.iter().sum::<usize>());
+}
+
+pub fn p1() {
+    solver(80);
+}
+pub fn p2() {
+    solver(256);
 }
